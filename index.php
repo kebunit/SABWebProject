5416559d2d1d37418a230673c12f9b33ed4f2efa
KEBUN IT <kebunit@gmail.com>
2015-04-15 13:28:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+<head>
+  <title>Site Name</title>
+</head>
+<body>
+  <div class="outer-container">
+    <div class="container" id="outer-page">
+      <div id="header">
+      </div>
+      <div id="content">
+      </div>
+      <div id="footer">
+      </div>
+    </div>
+  </div>
+</body>
+<html>
